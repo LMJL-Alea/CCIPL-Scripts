744be8acdd64619b8514402b3582b6f38e27b85c
AnthoOzier <anthony.ozier-lafontaine@eleves.ec-nantes.fr>
2021-12-16 11:28:37 +0100

    correction

--- tutoriel_CCIPL0.sh
+++ tutoriel_CCIPL0.sh
@@ -4,7 +4,10 @@ partition="SMP-short" # voir https://doc.ccipl.univ-nantes.fr/CCIPL-PUBLIC/stabl
 t="06:00:00" # temps maximal que devrait durer le run
 
 # divers paramètres 
+path=/home/LMJL/ozier-lafontaine-a/ # dossier où sont les scripts 
 diroutput=/scratch/LMJL/ozier-lafontaine-a/outputs # dossier dans lequel on enregistre les resultats
+mkdir -p ${diroutput} # créer le dossier des outputs
+
 int1=10
 int2=43
 n_jobs="10" # nombre de job paralleles sur un noeud 
@@ -43,60 +46,28 @@ done
 
 
 
-path=/home/LMJL/ozier-lafontaine-a/
-pathsc=/home/LMJL/ozier-lafontaine-a/human_cell_atlas_loom/
-diroutput=/scratch/LMJL/ozier-lafontaine-a/outputs
-mkdir -p ${diroutput}
 
 
+# # si tu veux tester le code sur ta machine avant de l'envoyer ça peut servir 
+# echo $PWD
+# local=false
+# if  [ $PWD = "/home/LMJL/ozier-lafontaine-a" ]; then
+#     echo CCIPL
+#     local=false
+# elif [ $PWD = "/home/anthony/These/Implementations/kfdapy" ]; then
+#     echo Local
+#     local=true
+# fi
 
 
-# si tu veux tester le code sur ta machine avant de l'envoyer ça peut servir 
-echo $PWD
-local=false
-if  [ $PWD = "/home/LMJL/ozier-lafontaine-a" ]; then
-    echo CCIPL
-    local=false
-elif [ $PWD = "/home/anthony/These/Implementations/kfdapy" ]; then
-    echo Local
-    local=true
-fi
-
-# liste des paramètres booléens utilisés par mon code python rangés en "parametre1:valeur1+parametre2:valeur2" 
-# Dans mon code python une fonction sait lire cette liste de paramètres 
-bool=bootstrap:${bootstrap}+sc:${sc}+local:${local}
-seeds=$(seq 0 100 1000) # de 0 a 1000 avec un pas de 100 (=[0,100,200,300,400,500,600,700,800,900,1000])
-nj="20" # nombre de jobs en parallèle 
-
-if [ "$local" = "true" ]; then
-    path=/home/anthony/These/Implementations/kfdapy/ # dossier contenant les fonctions à charger 
-    pathsc=/home/anthony/These/Implementations/data/2020_10_human_cell_atlas_loom/ # dossier contenant les données à charger 
-    diroutput=/home/anthony/These/Implementations/data/simu_local_01_2021/ # dossier où ecrire les fichier en sortie 
-    mkdir -p ${diroutput} # crée le dossier diroutput
-else
-    path=/home/LMJL/ozier-lafontaine-a/kfdapy/
-    pathsc=/home/LMJL/ozier-lafontaine-a/human_cell_atlas_loom/
-    diroutput=/scratch/LMJL/ozier-lafontaine-a/outputs
-    mkdir -p ${diroutput}
-fi
-
-for seed in $seeds
-do
-    jobname=seed${seed}
-    
-   if [ "$local" = "true" ]; then
-        # en local je lance juste le code python avec le paramètre bool nommé (par moi) -b
-        python ${path}main.py -b $bool
-    else
-        # je vérifie que le fichier que je compte écrire n'est pas déjà écrit afin d'éviter de faire tourner deux fois le même code pour la même seed 
-        if [ ! -e ${diroutput}${jobname} ]; then 
-            # sur le CCIPL je soumets un fichier .sh au gestionnaire slurm avec la commande sbatch
-            echo ${jobname} submitted
-            sbatch --job-name=${jobname}_${partition}_nj${nj} --time=$t -p $partition ${path}tutoriel_CCIPL1.sh $path $diroutput $jobname $bool $nj $partition $seed >log_${jobname}seed${seed} 2>&1
-            # les parametres que je spécifie à sbatch sont : --job-name, --time, -p
-            # le script .sh a lire par la commande sbatch est  ${path}1_simu.sh
-            # les paramètres que recevra le script .sh sont $path $diroutput $jobname $bool $nj $partition ( ça me permet de garder en mémoire toutes les infos utiles pour la reproductibilité)
-            # tous les prints et erreurs du script tutoriel_CCIPL1.sh seront écrits dans le fichier log_${jobname}seed${seed} (utile pour suivre ce qui se passe pendant le run )
-        fi
-    fi
-done
+# if [ "$local" = "true" ]; then
+#     path=/home/anthony/These/Implementations/kfdapy/ # dossier contenant les fonctions à charger 
+#     pathsc=/home/anthony/These/Implementations/data/2020_10_human_cell_atlas_loom/ # dossier contenant les données à charger 
+#     diroutput=/home/anthony/These/Implementations/data/simu_local_01_2021/ # dossier où ecrire les fichier en sortie 
+#     mkdir -p ${diroutput} # crée le dossier diroutput
+# else
+#     path=/home/LMJL/ozier-lafontaine-a/kfdapy/
+#     pathsc=/home/LMJL/ozier-lafontaine-a/human_cell_atlas_loom/
+#     diroutput=/scratch/LMJL/ozier-lafontaine-a/outputs
+#     mkdir -p ${diroutput}
+# fi
